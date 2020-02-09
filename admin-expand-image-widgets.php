0269735ab3d3308054f458f05636a0c1e82c4ad5
Walter Lopez <walter@wpallied.com>
2020-02-08 18:50:16 -0600

    Initial Commit

---
+++ admin-expand-image-widgets.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: Admin Expand Image Widgets
+ * Description: Auto-expand image widgets on Dashboard Widgets page, making images visible.
+ * Author: WPJohnny
+ * Author URI: https://wpjohnny.com/
+ * Version: 0.1.1
+ * Text Domain: admin-expand-image-widgets
+ * Domain Path: /languages/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+add_action( 'admin_footer-widgets.php', 'aeiw_print_script' );
+/**
+ * Print the script to expand the widgets.
+ *
+ * @since 0.1.0
+ * @return void
+ */
+function aeiw_print_script() {
+	?>
+
+	<script type="text/javascript">
+		jQuery(document).ready(function() {
+			jQuery('.widget-liquid-right div[id*="media_image"]').each(function(){
+				jQuery(this).find('.widget-title-action').trigger('click');
+			});
+		});
+	</script>
+
+	<?php
+}
---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== Restricted Site Access ===
+Contributors: treeflips
+Donate link: https://wordpress.org/plugins/admin-expand-image-widgets/
+Tags: widgets, images
+Requires at least: 4.9
+Tested up to: 5.3
+Stable tag: 0.1.1
+
+Auto-expand image widgets from Widgets page, making images visible.
+
+== Description ==
+
+
+
+== Installation ==
+
+1. Install easily with the WordPress plugin control panel or manually download the plugin and upload the extracted folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 0.1.1 =
+* Add compatibility with WordPress 5.3
